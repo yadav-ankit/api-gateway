c16549b1b36ff9b703760b4d4f18df8561a09463
any65889@gmail.com <ankyadav@amdocs.com>
2020-01-05 16:33:40 +0530

    Security added

--- src/main/java/com/microservice/phonekart/apigateway/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/microservice/phonekart/apigateway/config/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,22 @@
 package com.microservice.phonekart.apigateway.config;
 
-public class JwtAuthenticationEntryPoint {
+import java.io.IOException;
+import java.io.Serializable;
 
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements Serializable {
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
 }
--- src/main/java/com/microservice/phonekart/apigateway/controller/RequestHandlingController.java
+++ src/main/java/com/microservice/phonekart/apigateway/controller/RequestHandlingController.java
@@ -1,5 +1,13 @@
 package com.microservice.phonekart.apigateway.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequestHandlingController {
 
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
 }
