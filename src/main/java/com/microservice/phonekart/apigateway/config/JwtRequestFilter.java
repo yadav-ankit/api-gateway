fe008c230f0c957e3bfea05605497fd0eb88c080
any65889@gmail.com <ankyadav@amdocs.com>
2020-01-05 16:34:56 +0530

    Security added

--- src/main/java/com/microservice/phonekart/apigateway/config/JwtRequestFilter.java
+++ src/main/java/com/microservice/phonekart/apigateway/config/JwtRequestFilter.java
@@ -12,12 +12,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.microservice.phonekart.apigateway.service.JwtUserDetailsService;
 
 import io.jsonwebtoken.ExpiredJwtException;
 
-public class JwtRequestFilter {
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
 
 	@Autowired
 	private JwtUserDetailsService jwtUserDetailsService;
